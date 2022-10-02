c22f587e9649258bc25a4e112a511a00aed5b416
wesngu28 <serpents123@gmail.com>
2022-10-02 14:22:36 -0700

    some refactoring

--- packin.ts
+++ packin.ts
@@ -1,9 +1,13 @@
+#!/usr/bin/env npx ts-node
+
 import { Octokit } from "octokit"
 import * as dotenv from 'dotenv'
 import path from 'path'
 import { parse } from 'node-html-parser';
 import { readFileSync, readdirSync, lstatSync } from 'fs'
 import fetch from 'node-fetch'
+const { Input, Confirm  } = require('enquirer');
+
 dotenv.config()
 
 // Point a directory
@@ -12,8 +16,7 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 })
 
-let packageJsonLocationProbable = '';
-const searchPackageJson = (dir = process.cwd()) => {
+const searchPackageJson = (dir = process.cwd(), packageJsonLocation: string = '') => {
   const files = readdirSync(dir);
   for (const file of files) {
     const absolute = path.join(dir, file)
@@ -21,26 +24,74 @@ const searchPackageJson = (dir = process.cwd()) => {
       continue
     }
     if (absolute.includes('package.json')) {
-      packageJsonLocationProbable = path.resolve(absolute);
-      return;
+      packageJsonLocation = path.resolve(absolute)
     }
     if (lstatSync(absolute).isDirectory()) {
-      searchPackageJson(absolute)
+      searchPackageJson(absolute, packageJsonLocation)
     }
   }
+  return packageJsonLocation
 };
-searchPackageJson()
-const { Input } = require('enquirer');
+
+const buildRepoList = async (dependencies: string[]) => {
+  const repoList = dependencies.map(async(dependency: string)=> {
+    const response = await fetch(dependency);
+    const text = await response.text();
+    const html = parse(text);
+    return html.querySelector('#repository-link')?.text
+  })
+  return await Promise.all(repoList);
+}
+
+const checkStarStatus = async () => {
+  const repos = await octokit.request('GET /users/wesngu28/starred')
+  const starredRepoList = repos.data.map((repo: any) => {return repo.html_url})
+  return starredRepoList;
+}
+
+const getRepoNames = async (ghrepoLinks: string[]) => {
+  const repoNames = ghrepoLinks.map(async(repo: string) => {
+    const authorSlashRepo = repo.replace('https://github.com/', '')
+    const split = authorSlashRepo.split('/')
+    const repoResponse = await octokit.request(`GET /repos/${split[0]}/${split[1]}`)
+    return repoResponse.data.name
+  })
+  return await Promise.all(repoNames)
+  // for (let i = 0; i < ghrepoLinks.length; i++) {
+  //   const testes = ghrepoLinks[i].replace('https://github.com/', '')
+  //   const testeses = testes.split('/')
+  //   const repoResponse = await octokit.request(`GET /repos/${testeses[0]}/${testeses[1]}`)
+  //   repoNames.push(repoResponse.data.name);
+  // }
+}
+const packageJsonLocation = searchPackageJson(process.cwd())
+
+
+const askUsePackage = new Confirm({
+  name: 'existingPackage',
+  message: 'A package.json was detected in your current folder. Would you like to use it?'
+})
 
 const askLocation = new Input({
   name: 'location',
   message: 'What is the absolute path of your package json?'
 })
 
+const askPermission = new Confirm({
+  name: 'question',
+  message: 'Would you like to confirm each package before starring?'
+})
+
 const run = async (absolutePath: string) => {
-  if (!absolutePath) {
+  if (absolutePath) {
+    const usePackage = await askUsePackage.run();
+    if (!usePackage) {
+      absolutePath = await askLocation.run();
+    }
+  } else {
     absolutePath = await askLocation.run();
   }
+  const perm: boolean = await askPermission.run();
   if (!absolutePath.includes('package.json')) console.log('Not sure if this is a package.json but continuing anyway...')
   const dependenciesNPMUrl: string[] = []
   let jsonString = readFileSync(absolutePath);
@@ -50,59 +101,57 @@ const run = async (absolutePath: string) => {
     dependenciesNPMUrl.push(`https://www.npmjs.com/package/${dependencies}`)
   })
   const devDependencies = Object.keys(packager.devDependencies)
-  const npmScrapeResult: string[] = [];
-  async function buildRepoList(dependenciesArr: string[]) {
-    for(let i = 0; i < dependenciesArr.length; i++) {
-      const response = await fetch(dependenciesArr[i]);
-      const text = await response.text();
-      const html = parse(text);
-      const githubLink = html.querySelector('#repository-link')?.text
-      npmScrapeResult.push(`https://${githubLink!}`)
-    }
-  }
-  let haveYouStarred: string[] = [];
+  devDependencies.forEach(devDependency => {
+    dependenciesNPMUrl.push(`https://www.npmjs.com/package/${devDependency}`)
+  })
   const githubRepos = await buildRepoList(dependenciesNPMUrl);
-  async function checkStarStatus() {
-    const repos = await octokit.request('GET /users/wesngu28/starred')
-    haveYouStarred = repos.data.map((repo: any) => {return repo.html_url})
-    console.log(haveYouStarred)
-  }
-  const stars = await checkStarStatus()
-  const repoNames: string[] = [];
-  async function getRepoNames() {
-    for (let i = 0; i < npmScrapeResult.length; i++) {
-      const testes = npmScrapeResult[i].replace('https://github.com/', '')
-      const testeses = testes.split('/')
-      const repoResponse = await octokit.request(`GET /repos/${testeses[0]}/${testeses[1]}`)
-      repoNames.push(repoResponse.data.name);
-    }
-  }
-  await getRepoNames()
-  console.log(repoNames)
-  let starStatus = npmScrapeResult.map((dependency, idx) => {
-    console.log(dependency)
-    if(haveYouStarred.includes(dependency)) {
+  const starredRepos = await checkStarStatus()
+  const repoNames = await getRepoNames(starredRepos)
+  console.log(starredRepos)
+  const starStatus = githubRepos.map((dependency, idx) => {
+    const append = `https://${dependency}`
+    console.log(append)
+    if(starredRepos.includes(append!)) {
       console.log(`You have already starred ${repoNames[idx]}`)
     } else {
       console.log(`You have yet to star ${repoNames[idx]}`)
-      return dependency;
+      return append;
     }
   })
-  starStatus = starStatus.filter(repo => repo !== undefined)
-  starStatus.forEach(async (repo) => {
-    const testes = repo!.replace('https://github.com/', '')
-    const testeses = testes.split('/')
-    const starMe = await octokit.request('PUT /user/starred/{owner}/{repo}', {
-      owner: testeses[0],
-      repo: testeses[1]
-    })
-    console.log('Starred ' + repo)
-  });
+  let filteredStarStatus = starStatus.filter(repo => repo !== undefined)
+  const removeDuplicate = [...new Set(filteredStarStatus)];
+  filteredStarStatus = Array.from(removeDuplicate)
+  for (const repo of filteredStarStatus) {
+    console.log(repo)
+    const authorSlashRepo = repo!.replace('https://github.com/', '')
+    const split = authorSlashRepo.split('/')
+    console.log(split)
+    if(perm) {
+      const askStar = new Confirm({
+        name: 'question',
+        message: `Would you like to star ${split[1]} by ${split[0]}`
+      })
+      const confirmation = await askStar.run()
+      if(confirmation) {
+        const starMe = await octokit.request('PUT /user/starred/{owner}/{repo}', {
+          owner: split[0],
+          repo: split[1]
+        })
+        console.log('Starred ' + repo)
+      }
+    } else {
+      const starMe = await octokit.request('PUT /user/starred/{owner}/{repo}', {
+        owner: split[0],
+        repo: split[1]
+      })
+      console.log('Starred ' + repo)
+    }
+  }
   console.log(starStatus)
   console.log(`Your package json is located ${absolutePath}`);
 }
-
-run(packageJsonLocationProbable)
+console.log(packageJsonLocation)
+run(packageJsonLocation!)
 
 // const getAllRepos = async () => {
 //   const repos = await octokit.request('GET /users/wesngu28/starred', {
